812a8445a9860f6acc7d72b371f8edd58d3400e8
Yukiya Ogasa <yodog712@gmail.com>
2018-08-20 10:53:02 +0900

    インデント修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,6 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    aaa = 'aaa'
   end
 
 end
