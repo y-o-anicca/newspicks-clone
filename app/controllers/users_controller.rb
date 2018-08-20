913b81cf01817b70af25c37b398ed9958994cf80
root <root@newspicks1.localdomain>
2018-08-20 04:09:49 +0000

    create login_form and session controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user 
       flash[:success] = "ユーザー登録完了"
-      redirect_to @user 
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   root 'top_pages#home'
   get '/signup' => 'users#new'
   resources :users
