3ac4a580304a46c277e5a4fbe79a8411bf8b9b8d
Yukiya Ogasa <yodog712@gmail.com>
2018-08-20 11:28:21 +0900

    インデント修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- 	root 'top_pages#home'
- 	get '/signup' => 'users#new'
- 	resources :users
+　root 'top_pages#home'
+　get '/signup' => 'users#new'
+　resources :users
 end
