308fbea5c778cfd08068faf067c374e4647d2409
root <root@newspicks1.localdomain>
2018-08-20 03:31:51 +0000

    create signup_form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,28 @@ class UsersController < ApplicationController
   def new
   end
 
-  def show 
+  def show
   	@user = User.find(params[:id])
   end
 
-  def new 
+  def new
   	@user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録完了"
+      redirect_to @user 
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
